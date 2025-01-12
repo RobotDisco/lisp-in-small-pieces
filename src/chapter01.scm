df24ec7e678103561d2ff786c1c0b77d9507c761
Gaelan D'costa <gdcosta@gmail.com>
2025-01-11 22:20:48 -0500

    Ex 1.7 -- Implement continuations
    Squashed commit of the following:
    
    commit b83439890fb37c86eb5fc11245b359da20e04118
    Author: Gaelan D'costa <gdcosta@gmail.com>
    Date:   Sat Jan 11 22:15:50 2025 -0500
    
        Implement continuations in function application
    
    commit e07951d0d5795c1ceac1d16605fb053ead0808b4
    Author: Gaelan D'costa <gdcosta@gmail.com>
    Date:   Wed Jan 1 14:25:46 2025 -0500
    
        Implement (lambda ...) evaluation in continuation form
    
    commit ccc9d9a3eaa4d3e6b1d0f3f4d28342774b7367bc
    Author: Gaelan D'costa <gdcosta@gmail.com>
    Date:   Wed Jan 1 13:53:10 2025 -0500
    
        Implement set! with continuations.
    
    commit a309f87dfe932de3237f81ff354c62117b6f73cb
    Author: Gaelan D'costa <gdcosta@gmail.com>
    Date:   Wed Jan 1 13:41:33 2025 -0500
    
        Properly implement eprogn for continuations
    
    commit a7702385b1066086c3f3f0ae35cab26fb903cd7a
    Author: Gaelan D'costa <gdcosta@gmail.com>
    Date:   Sun Dec 29 20:23:28 2024 -0500
    
        Attempting to implement continuations

--- src/chapter01.scm
+++ src/chapter01.scm
@@ -12,15 +12,46 @@
 ;;;; reusing the host implementations.
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
+;; Exercise 1.7
+;; Trivial continuation - return result as-is
+(define (trivial-cont val)
+  val)
+
 ;; Home-made evaluator, leaniing heavily on host Scheme implemntation.
 ;; expr - Expression to evaluate
 ;; env - Environment to inherit
-(define (evaluate expr env)
+;; Exercise 1.7: Implement call-with-current continuation i.e. `call/cc'.
+;;   Continuations are scheme's way of doing things like exception handling,
+;;   although in truth they are more generic and almost as powerful as GOTO
+;;   without being as unregulated (there's at least _some_ control of how the)
+;;   computation jumps work. The way this happens is that the evaluate function
+;;   uses something called "continuation-passing-style", which means that
+;;   alongside the current environment bindings, we also pass into evaluate a
+;;   callback-like function (called the current continuation) that we apply to
+;;   the result of our current evaluation. When using a primitive like "call/cc",
+;;   we can change what the current continuation is. This allows us to set up
+;;   things like exception handlers, or some people use them to implement
+;;   generators.
+;;
+;;   As far as I call tell this is what happens when you have minimal syntax and
+;;   need to expicitly extend lambda calculus evaluation rules to implement
+;;   something like an exception i.e. breaking out the normal evaluation flow.
+;;   Although in this case we literally have to modify our evaluation logic
+;;   to support the capability by _always_ passing our evaluation result to
+;;   our current continuation and supplying a really trivial starting
+;;   continuation (i.e. return the value as is unmodified) to start.
+;;
+;;   A REALLY WEIRD TWIST about continuations is that we're not relying at all
+;;   on a return value ... we're using `cont' as a callback, essentially, and
+;;   it is what will be returning the final value ultimately.
+;; cont - The current continuation i.e. the remainder of the program after this
+;;     point. Basically a callback to apply to our evaluation result thus far.
+(define (evaluate expr env cont)
   ;; We're not a composite / list-based expression.
   (if (atom? expr)
       (cond
        ;; Are we a variable? Well then, see if we have an associated value.
-       ((symbol? expr) (lookup expr env))
+       ((symbol? expr) (cont (lookup expr env)))
        ;; Are we a primitive atom? OK then, auto-unquote to get the real value.
        ((or (number? expr)
 	    (string? expr)
@@ -28,7 +59,7 @@
 	    (boolean? expr)
 	    (vector? expr))
 	;; Autounquoting is basically returning the expr as-is.
-	expr)
+	(cont expr))
        ;; This is an invalid atom expression.
        (else (wrong "Cannot evaluate expression" expr)))
       ;; OK I guess we are a list-based expression.
@@ -37,41 +68,77 @@
 	;; We start by looking at all the special forms, with special evaluation
 	;; behaviour.
 	;; You're a quoted list, so just return the next item.
-	((quote) (cadr expr))
+	((quote) (cont (cadr expr)))
 	;; You're a conditional, so evaluate the test condition, then evaluate
 	;; and return the appropriate branch.
 	;;
 	;; Note that we match to our language dialect's false value, using
 	;; eq? as our false value is a singleton value, so comparing memory
 	;; location of the values makes sense.
-	((if) (if (not (eq? (evaluate (cadr expr) env) the-false-value))
-		  (evaluate (caddr expr) env)
-		  (evaluate (cadddr expr) env)))
+	((if) (if (not (eq? (evaluate (cadr expr)
+				      env
+				      trivial-cont) the-false-value))
+		  (evaluate (caddr expr) env cont)
+		  (evaluate (cadddr expr) env cont)))
 	;; You're a begin/progn, a sequence of expressions to run.
-	((begin) (eprogn (cdr expr) env))
+	((begin) (eprogn-continuation (cdr expr) env cont))
 	;; You're a mutation, which is a side-effect.
-	((set!) (update! (cadr expr) env (evaluate (caddr expr) env)))
+	;; ((set!) (update! (cadr expr) env (evaluate (caddr expr) env)))
+	;;
+	;; Ex 1.7 -- Remember, we're using a continuation callback. So we
+	;; can't rely on return values, we have to leverage callbacks. This
+	;; is your clue as to whether you need a specialized contuation or
+	;; the one currently passed in.
+	((set!) (evaluate (caddr expr) env
+			  (lambda (res)
+			    ;; Update environment with result as a side-effect
+			    ;; (cadr expr) still indicates our key.
+			    (update! (cadr expr) res env)
+			    ;; We return the evaluated value on set! so now
+			    ;; apply our current continuation to the evaluated
+			    ;; value.
+			    (cont res))))
 	;; You're an (anonymous) function definition.
-	((lambda) (make-function (cadr expr) (cddr expr) env))
+	((lambda) (make-function (cadr expr) (cddr expr) env) cont)
 	;; Use the standard evaluate form, invoke the function associated with
 	;; the first symbol, applied to the arguments that follow.
 	(else
 	 (let ((fn (car expr))
-	       (args (evlis (cdr expr) env)))
+	       ;; (args (evlis (cdr expr) env))
+	       )
 	   (begin
-	     ;; Exercise 1.1: Rudimentary tracer
-	     ;; Print the function name and the arguments
-	     (display (list "FN-ENTER:" fn "args:" args))
-	     (newline)
-	     (let
-		 ;; Call the function and supply the arguments
-		 ((res (invoke (evaluate fn env)
-			       args)))
-	       ;; Exercise 1.1: Rudimentary tracer
-	       ;; Print the function name and the result
-	       (display (list "FN-EXIT:" fn "result:" res))
-	       (newline)
-	       res)))))))
+	     ;; Ex 1.7 Evaluate the function and apply it to our supplied
+	     ;; arguments, done via continuation callback
+	     (evlis-continuation-tco
+	      (cdr expr)
+	      env
+	      (lambda (args)
+		;; Exercise 1.1: Rudimentary tracer
+		;; Print the function name and the arguments
+		(display (list "FN-ENTER:" fn "args:" args))
+		(newline)
+		(evaluate fn env (lambda (fnval)
+				   ;; Exercise 1.1: Rudimentary tracer
+				   ;; Print the function name and the result
+				   (invoke fnval args env
+					   (lambda (res)
+					     (display (list
+						       "FN-EXIT:"
+						       fn
+						       "result:"
+						       res))
+					     (newline)
+					     (cont res)))))))
+	     ;; (let
+	     ;; 	 ;; Call the function and supply the arguments
+	     ;; 	 ((res (invoke (evaluate fn env)
+	     ;; 	  	       args)))
+	     ;;   ;; Exercise 1.1: Rudimentary tracer
+	     ;;   ;; Print the function name and the result
+	     ;;   (display (list "FN-EXIT:" fn "result:" res))
+	     ;;   (newline)
+	     ;;   res)
+	     ))))))
 
 
 ;; An arbitrary value we use to represend false in our home-made Scheme dialect
@@ -83,8 +150,8 @@
 
 ;; Evaluate a sequence of expressions. If the sequence is empty, return an
 ;; empty list.
-;; exprs - Expresions to evaluate
-;; env - Envirornment to inherit
+;; exprs - Expressions to evaluate
+;; env - Environment to inherit
 (define (eprogn exprs env)
   ;; Are we a list of expressions?
   (if (pair? exprs)
@@ -96,12 +163,44 @@
 	  ;; Note that the (begin) we use here is part of the host scheme
 	  ;; implementation.
 	  (begin (evaluate (car exprs) env)
-		 (eprogn (cdr exprs) env))
+	  	 (eprogn (cdr exprs) env))
 	  ;; We're at the last expression in the list, so return the value of
 	  ;; its evaluation.
 	  (evaluate (car exprs) env))
       ;; We weren't a list so we must have been an empty list, so return that.
-      '()))
+      (cont '())))
+
+;; Evaluate a sequence of expressions. If the sequence is empty, return an
+;; empty list.
+;; exprs - Expressions to evaluate
+;; env - Environment to inherit
+;; cont - Ex 1.7: current continuation function
+(define (eprogn-continuation exprs env cont)
+  ;; Are we a list of expressions?
+  (if (pair? exprs)
+      ;; Do we have an additional expression after this first one?
+      (if (pair? (cdr exprs))
+	  ;; Ok, evaluate the first expression and then recursively run the next
+	  ;; one as part of the system.
+	  ;;
+	  ;; Note that the (begin) we use here is part of the host scheme
+	  ;; implementation.
+	  ;;
+	  ;; Ex 1.7 Using continuations makes this easier, because we can
+	  ;; evaluate the head expression for its side-effect, then use an
+	  ;; explicit continuation that ignores the result and evaluates the
+	  ;; next s-expression in the eprogn chain.
+	  (evaluate (car exprs) env
+		    (lambda _
+		      (eprogn-continuation (cdr exprs) env cont)))
+	  ;; We're at the last expression in the list, so return the value of
+	  ;; its evaluation.
+	  ;; Ex 1.7 -- Since this is our final value, apply our current
+	  ;; continuation to the result
+	  (evaluate (car exprs) env cont))
+      ;; We weren't a list so we must have been an empty list, so return that.
+      ;; Or for Ex 1.7 -- apply our current continuation to the empty list.
+      (cont '())))
 
 
 ;; Utility function that takes a list of expressions and returns the
@@ -124,6 +223,81 @@
 		(evlis (cdr exprs) env)))
       ;; Otherwise, just return an empty list.
       '()))
+;; Ex 1.7 - Continuation callbacks make this weird, as we have to recurse via
+;; callback but also preserve the order. This is going to be hard to follow.
+;; exprs - List of expressions
+;; env ; Environment to inherit
+;; cont - Ex 1.7: current continuation function
+(define (evlis-continuation exprs env cont)
+  ;; Are we at the end of the list?
+  (if (null? exprs)
+      ;; Our base case, where the recursion stops.
+      ;;
+      ;; Pass the empty list (the list end) to the callback, which maybe not be
+      ;; the original continuation but the one we create in our our recursion
+      ;; (see the lambda below)
+      (cont '())
+      ;; Otherwise, evaluate the head of the list, then recursively handle
+      ;; the rest of the list. We use a custom continuation to handle the
+      ;; recursion and keep the proper order.
+      (evaluate (car exprs)
+		env
+		(lambda (first)
+		  (evlis-continuation (cdr exprs)
+				       env
+				       (lambda (rest)
+					 ;; This looks weird, but remember that
+					 ;; in a recursive call to evlist, so
+					 ;; the lambda isn't necessary the one
+					 ;; passed in originally. This cont
+					 ;; instead will get passed up the chain
+					 ;; with the original one only called
+					 ;; when the list has been fully consed.
+					 (cont (cons first rest))))))))
+;; The above function is terrible for tail recursion. Making it handle tail
+;; recursion well will make this function even more obtuse.
+(define (evlis-continuation-tco exprs env cont)
+  ;; We need to use an iterative helper function to handle this, the form we
+  ;; currently have doesn't have an accumulation value we can lead on for
+  ;; tail-call-optimization
+  ;;
+  ;; So call it, providing an empty list we will extend.
+  (evlis-cont-tco-acc exprs env '() cont))
+;; Accmumlating helper function for an evlist based on continuations.
+;; This relies on Scheme's tail-call-optimization because, as you can see
+;; we can lean on the recursive callback repeatedly resolving to
+;; a straight call to ev-list-const-tco-acc, which means we can just iterating
+;; on the last value in the stack rather than growing the call stack for each
+;; recursion. Remember the last expression in our continuation-using evaluate
+;; is basically (k val) which will become (evlis-cont-tco-acc ...).
+;;
+;; exprs - List of expressions
+;; env - Environment to inherit
+;; acc - Accumulator which gathers our list (in reverse)
+;; cont - Ex 1.7: current continuation function
+(define (evlis-cont-tco-acc exprs env acc cont)
+  ;; Are we at the end of the list?
+  (if (null? exprs)
+      ;; Our base case, where the recursion stops.
+      ;;
+      ;; Our accumulated list is in the wrong order because we can only add to
+      ;; the front of the but we processed values from first-to-last, so our
+      ;; accumulator is currently in last-to-first sequence.
+      ;; Reverse that before passing it to our original call-back.
+      (cont (reverse acc))
+      ;; Otherwise, evaluate the head of the list
+      (evaluate (car exprs)
+		env
+		(lambda (first)
+		  ;; Iterative recursion
+		  (evlis-cont-tco-acc
+		   ;; Our remaining expr list keeps srinking
+		   (cdr exprs)
+		   env
+		   ;; And our accumulator keeps growing
+		   (cons first acc)
+		   ;; And pass the original continuation
+		   cont)))))
 
 
 ;; Looks up a key in the environment alist, and return the corresponding value.
@@ -285,12 +459,12 @@
 ;;
 ;; fn - Function to call
 ;; args - arguments to apply
-(define (invoke fn args)
+(define (invoke fn args env cont)
   (display (list "FN-IN:" fn "args:" args))
   ;; Are you a recognized procedure in the host scheme runtime?
   (if (procedure? fn)
       ;; Apply the function to the supplied arguments
-      (fn args)
+      (fn args env cont)
       ;; Not a function, error out.
       (wrong "Attempting to apply non-function " fn)))
 
@@ -301,10 +475,12 @@
 ;; variables the list of variable expressions to apply the body to
 ;; body - the expression that is the body of the function
 ;; env - the enviironment to inherit
+;; cont - Ex 1.7: current continuation function
+;;
 (define (make-function variables body env)
   (lambda (values)
     ;; The body of a function is effectively an implicit begin/progn
-    (eprogn body (extend env variables values))))
+    (eprogn body (extend env variables values) cont)))
 
 
 ;; The empty environment is just an empty list
@@ -336,13 +512,14 @@
 ;; that doesn't confirm to that arity.
 (define-syntax defprimitive
   (syntax-rules ()
-    ((defprimitive name fn arity)
+    ((defprimitive name arity fn)
      (definitial name
-       (lambda (values)
-	 (if (= arity (length values))
-	     (apply fn values)
+       ;; Ex 1.7 Continuations wind up infecting here as well
+       (lambda (args env cont)
+	 (if (= arity (length args))
+	     (fn args env cont)
 	     (wrong "Function invoked with arglist of wrong arity: "
-		    (list name values))))))))
+		    (list name args))))))))
 
 
 ;; Populate our global environment with basic boolean and nil variables
@@ -369,19 +546,42 @@
 
 ;; Populate some functions from our host runtime, providing a value
 ;; for our arity-checker.
-(defprimitive cons cons 2)
-(defprimitive car car 1)
-(defprimitive set-cdr! set-cdr! 2)
-(defprimitive + + 2)
-(defprimitive eq? eq? 2)
+;; (defprimitive cons 2 cons 2)
+;; (defprimitive car car 1)
+;; (defprimitive set-cdr! set-cdr! 2)
+;; (defprimitive + + 2)
+;; (defprimitive eq? eq? 2)
+;;
+;; Ex 1.7 This becomes more annoying when we use continuations, we can't rely on
+;; just passing to the hosting runtime's function as is. It's going to be a lot
+;; of busywork reconstituting the argument and applying it to the hosting
+;; runtime's functions.
+(defprimitive cons 2
+  (lambda (args env cont)
+    (cont (cons (car args) (cdr args)))))
+(defprimitive car 1
+  (lambda (args env cont)
+    (cont (car args))))
+(defprimitive set-cdr! 2
+  (lambda (args env cont)
+    (set-cdr! (car args) (cadr args))))
+(defprimitive + 2
+  (lambda (args env cont)
+    (cont (+ (car args) (cadr args)))))
+(defprimitive eq? 2
+  (lambda (args env cont)
+    (cont (eq? (car args) (cadr args)))))
+
+
 ;; Ex 1.5 - Don't just return the host platform's boolean value, use
 ;; the custom boolean values we have invented.
 ;; Ye original defprimitive
 ;; (defprimitive < < 2)
-(defprimitive < (lambda (a b)
-		  (if (< a b)
-		      (evaluate 't env.global)
-		      (evaluate 'f env.global))) 2)
+(defprimitive < 2
+  (lambda (args env cont)
+    (if (< (car args) (cadr args))
+	(evaluate 't env.global cont)
+	(evaluate 'f env.global cont))))
 ;; Ex 1.6 - Implement (list)
 ;; This relies on "implicit dot notation":
 ;; (lambda arg arg) returns the whole argument list, compared to
@@ -411,7 +611,8 @@
 ;; -> ((lambda (1 2 3 4 5) (1 2 3 4 5)))
 ;; -> (1 2 3 4 5)
 (definitial list
-  (apply (lambda lst lst) values))
+  (lambda (args env cont)
+    (cont (apply (lambda lst lst) args))))
 
 ;; This is our chapter's main entry-point that brings up a really simple REPL.
 (define (chapter01-scheme)
@@ -428,7 +629,9 @@
 		 (newline))
 	  (begin
 	    ;; The Evaluate and Print section of the REPL loop.
-	    (display (evaluate input env.global))
+	    ;; Note that we supply a starting continuation to just return the
+	    ;; evaluated value as-is.
+	    (display (evaluate input env.global trivial-cont))
 	    (newline)
 	    ;; Here our REPL leans on tail recursion to Loop.
 	    (toplevel)))))
